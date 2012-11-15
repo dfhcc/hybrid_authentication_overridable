0cd8c789b7d8c867e7fe05854267ffcc9775d247
Scott Pullen <s.pullen05@gmail.com>
2012-11-15 14:28:12 -0500

    updating test dependencies

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,18 @@
 ENV['RAILS_ENV'] = 'test'
 
-require File.expand_path('rails_app/config/environment.rb',  File.dirname(__FILE__))
-require 'rspec/rails'
-require 'rspec/autorun'
+require 'active_record'
+require 'rspec'
 require 'factory_girl'
+require 'database_cleaner'
 
-require File.expand_path('../../lib/hybrid_authentication_overridable', File.dirname(__FILE__))
+require File.expand_path('../lib/hybrid_authentication_overridable', File.dirname(__FILE__))
 
 Dir[File.expand_path("support/**/*.rb", File.dirname(__FILE__))].each {|f| require f}
 
 RSpec.configure do |config|
   config.mock_with :rspec
-  config.use_transactional_fixtures = false
-  config.infer_base_class_for_anonymous_controllers = false
+  #config.use_transactional_fixtures = false
+  #config.infer_base_class_for_anonymous_controllers = false
   
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
