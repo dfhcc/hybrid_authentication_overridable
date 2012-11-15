ebaf19870404e92a504e5ca17c1f7009595976b7
Scott Pullen <s.pullen05@gmail.com>
2012-11-15 14:13:06 -0500

    removing rails app, try with standalone active record

--- spec/rails_app/app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ActiveRecord::Base
-  devise :ldap_authenticatable, :database_authenticatable, :rememberable, :trackable, :hybrid_authentication_overridable
-
-  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
-end
--- spec/rails_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-RailsApp::Application.config.secret_token = 'e49da0cfe9422e9a3a5dcfd2330f35dc6625c99d974adf275563a20278cdc29f3be890e197084b8eb2f9bb113b33b645df1961d10faa69b54fcfc2bbeba5309b'
